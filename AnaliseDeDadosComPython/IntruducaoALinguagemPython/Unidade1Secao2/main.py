41688ab4b01a0337a4040f8a2321ec6c6858131b
Yuri Gabriel <67286222+yurigabriel25@users.noreply.github.com>
2022-06-08 10:03:03 -0300

    addExerciciosUnidade1Secao2

---
+++ AnaliseDeDadosComPython/IntruducaoALinguagemPython/Unidade1Secao2/main.py
@@ -0,0 +1,32 @@
+# Exercício proposto no livro Python 3 - Conceitos e Aplicações Uma Abordagem Didática (Capítulo 2, pag. 47/48).
+# Operadores aritiméticos em Python:
+
+a = 14
+b = 5
+
+adicao = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+divisaoInterna = a // b
+restoModulo = a % b
+unario = -a
+potenciacao = a ** b
+
+print(f"O resultado da adição é: {adicao}")
+
+print(f"O resultado da subtração é: {subtracao}")
+
+print(f"O resultado da multiplicação é: {multiplicacao}")
+
+print(f"O resultado da divisão é: {divisao}")
+
+print(f"O resultado da divisão interna é: {divisaoInterna}")
+
+print(f"O resultado do resto (módulo) é: {restoModulo}")
+
+print(f"O resultado do - unário é: {unario}")
+
+print(f"O resultado da potenciação é: {potenciacao}")
+
+input("Digite qualquer tecla para finalizar.")
